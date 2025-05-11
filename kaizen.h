38abb5dd73776f4fb2d0f2790550f49acbe021c9
Leo <leo.heinsaar@hotmail.com>
2025-05-11 22:29:39 +0400

    Add `zen::fs` to local `kaizen.h`
    Also added to the official Kaizen library.

--- kaizen.h
+++ kaizen.h
@@ -312,6 +312,8 @@ namespace color {
 
 ///////////////////////////////////////////////////////////////////////////////////////////// FILESYSTEM
 
+namespace fs = std::filesystem;
+
 std::filesystem::path current_path() { return std::filesystem::current_path(); }
 std::filesystem::path  parent_path() { return std::filesystem::current_path().parent_path(); }
 
